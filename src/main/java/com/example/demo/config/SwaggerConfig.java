529f4b7a5c540bcc985d13da173ed711ff40e36e
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-02-20 23:54:08 +0900

    swagger 추가

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,53 +1,24 @@
-//package com.example.demo.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//import springfox.documentation.service.Contact;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .consumes(getConsumeContentTypes())
-//                .produces(getProduceContentTypes())
-//                .apiInfo(getApiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.ddaja.ddaja.controller"))
-//                .paths(PathSelectors.ant("/**"))
-//                .build();
-//    }
-//
-//    private Set<String> getConsumeContentTypes() {
-//        Set<String> consumes = new HashSet<>();
-//        consumes.add("application/json;charset=UTF-8");
-//        consumes.add("application/x-www-form-urlencoded");
-//        return consumes;
-//    }
-//
-//    private Set<String> getProduceContentTypes() {
-//        Set<String> produces = new HashSet<>();
-//        produces.add("application/json;charset=UTF-8");
-//        return produces;
-//    }
-//
-//    private ApiInfo getApiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("API")
-//                .description("[DDaJa] REST API")
-//                .contact(new Contact("[DDaja Swagger]", "https://github.com/swgil007/DDaJa", "BNG"))
-//                .version("1.0")
-//                .build();
-//    }
-//}
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Space Dog")
+                .description("Space Dog의 Swagger UI 테스트")
+                .version("1.0.0");
+    }
+}
