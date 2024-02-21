e23e105d1130f4fc30341a4839600160996cd24d
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-02-21 12:29:39 +0900

    밥먹기 전 커밋

--- src/main/java/com/example/demo/domain/Locate.java
+++ src/main/java/com/example/demo/domain/Locate.java
@@ -16,4 +16,36 @@ public class Locate {
     private String user_id;
     private Float longitude;
     private Float latitude;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
 }
---
+++ src/main/java/com/example/demo/dto/locate/LocateInfoDTO.java
@@ -0,0 +1,38 @@
+package com.example.demo.dto.locate;
+
+import com.example.demo.domain.Locate;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(title = "위치 정보")
+public class LocateInfoDTO {
+    private Float longitude;
+    private Float latitude;
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public Locate toLocate(String userId){
+        Locate l = new Locate();
+        l.setUser_id(userId);
+        l.setLatitude(this.latitude);
+        l.setLatitude(this.longitude);
+        return l;
+    }
+}
